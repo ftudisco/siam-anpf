71d7adc9b38ed70ba098e4c14ebc8e45f9c06029
ftudisco <tudisco.francesco@gmail.com>
2021-05-06 08:24:16 +0100

    Deployed 95fea6d with MkDocs version: 1.1.2

--- assets/mathjaxhelper.js
+++ assets/mathjaxhelper.js
@@ -11,16 +11,36 @@
 //   }
 // });
 window.MathJax = {
+  // loader: {load: ['[tex]/tagformat']},
   tex: {
     inlineMath: [["\\(", "\\)"],["$","$"]],
     displayMath: [["\\[", "\\]"],["$$","$$"]],
-    processEscapes: true,
-    processEnvironments: true
-    mtextFontInherit: true
+    processEscapes: true, 
+    tagSide: 'left',
+    macros: {
+      RR: '{\\mathbb R}'
+      // b: ['{\\mathbf #1}',1]
+    },
+    tags: 'ams'
   },
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
+  },
+  chtml: {
+    scale: .9,                      // global scaling factor for all expressions
+    minScale: .5,                  // smallest scaling factor to use
+    matchFontHeight: true,         // true to match ex-height of surrounding font
+    mtextInheritFont: true,       // true to make mtext elements use surrounding font
+    merrorInheritFont: false,      // true to make merror text use surrounding font
+    mtextFont: '',                 // font to use for mtext, if not inheriting (empty means use MathJax fonts)
+    merrorFont: 'serif',           // font to use for merror, if not inheriting (empty means use MathJax fonts)
+    unknownFamily: 'serif',        // font to use for character that aren't in MathJax's fonts
+    mathmlSpacing: false,          // true for MathML spacing rules, false for TeX rules
+    skipAttributes: {},            // RFDa and other attributes NOT to copy to the output
+    exFactor: .5,                  // default size of ex in em units
+    displayAlign: 'left',        // default for indentalign when set to 'auto'
+    displayIndent: '1em'             // default for indentshift when set to 'auto'
   }
 };
 
