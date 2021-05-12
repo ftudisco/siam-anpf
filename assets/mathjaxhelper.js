2ad5b7cec10a60e34b122d524d30c327e9681b1e
ftudisco <tudisco.francesco@gmail.com>
2021-05-12 17:22:46 +0100

    Deployed 902a861 with MkDocs version: 1.1.2

--- assets/mathjaxhelper.js
+++ assets/mathjaxhelper.js
@@ -11,14 +11,18 @@
 //   }
 // });
 window.MathJax = {
-  // loader: {load: ['[tex]/tagformat']},
   tex: {
     inlineMath: [["\\(", "\\)"],["$","$"]],
     displayMath: [["\\[", "\\]"],["$$","$$"]],
     processEscapes: true, 
+    processEnvironments: true,
     tagSide: 'left',
     macros: {
-      RR: '{\\mathbb R}'
+      RR: '{\\mathbb R}',
+      R: '{\\mathbb R}',
+      krog: '{\\odot}', 
+      M: '{\\mathit{\\Theta}}',
+      one: '{\\mathbf 1}'
       // b: ['{\\mathbf #1}',1]
     },
     tags: 'ams'
@@ -28,10 +32,10 @@ window.MathJax = {
     processHtmlClass: "arithmatex"
   },
   chtml: {
-    scale: .9,                      // global scaling factor for all expressions
+    scale: 1,                      // global scaling factor for all expressions
     minScale: .5,                  // smallest scaling factor to use
     matchFontHeight: true,         // true to match ex-height of surrounding font
-    mtextInheritFont: true,       // true to make mtext elements use surrounding font
+    mtextInheritFont: true,         // true to make mtext elements use surrounding font
     merrorInheritFont: false,      // true to make merror text use surrounding font
     mtextFont: '',                 // font to use for mtext, if not inheriting (empty means use MathJax fonts)
     merrorFont: 'serif',           // font to use for merror, if not inheriting (empty means use MathJax fonts)
@@ -39,8 +43,8 @@ window.MathJax = {
     mathmlSpacing: false,          // true for MathML spacing rules, false for TeX rules
     skipAttributes: {},            // RFDa and other attributes NOT to copy to the output
     exFactor: .5,                  // default size of ex in em units
-    displayAlign: 'left',        // default for indentalign when set to 'auto'
-    displayIndent: '1em'             // default for indentshift when set to 'auto'
+    displayAlign: 'left',          // default for indentalign when set to 'auto'
+    displayIndent: '0em'           // default for indentshift when set to 'auto'
   }
 };
 
